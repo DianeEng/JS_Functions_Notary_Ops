57c383978b9fef980cbd9dd7e6ae072586e8bf53
DianeEng <dicksonreview@outlook.com>
2023-01-22 20:40:04 -0500

    Header LayOut Change
    Changed header layout (ID)

---
+++ index.js
@@ -0,0 +1,13 @@
+ // The parenthsis although blank is apart of Syntax for declaring function
+    //In curly brackets in body of function to display message on console
+
+
+
+
+function greet(name, lastname) {
+    
+    console.log('Hello ' + name + " " + lastname );
+}
+
+greet('John',"Smith");
+
